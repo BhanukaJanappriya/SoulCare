c098741c63e4779c214b0161222699974e7eec3c
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-07-06 20:44:20 +0530

    V 2.0 User Dashboard

--- soulCare/src/App.tsx
+++ soulCare/src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import UserDashboard from './components/UserDashboard';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Appointments from './pages/Appointments/Appointments'
+import Activity from './pages/Activity/Activity'
 
-const App: React.FC = () =>{
-  return(
-    <div className="min-h-screen bg-pageBg text-bodyText font-sans">
-      <UserDashboard/>
-    </div>
-    
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-black flex items-center justify-center p-4">
+        <main className="w-full max-w-[1440px] h-[960px] bg-white rounded-4xl shadow-custom overflow-hidden">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/appointments" element={<Appointments />} />
+              <Route path="/activity" element={<Activity />} />
+            </Routes>
+          </Layout>
+        </main>
+      </div>
+    </Router>
   )
 }
 
-export default App;
+export default App
