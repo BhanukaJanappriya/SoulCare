cb53e8bd61910fa945cdbb111ba018d970acc00d
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-07-06 15:22:31 +0530

    base theme using color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        divider: '#D6EAF8',
+        error: '#E74C3C',
+        warning: '#F39C12',
+        success: '#48C9B0',
+        labelText: '#6C7A89',
+        bodyText: '#2C3E50',
+        heading: '#144A74',
+        cardBg: '#F0F9FF',
+        pageBg: '#E3F2FD',
+        secondaryBtn: '#A7D8F0',
+        primaryBtn: '#5DADEC',
+      },
+      fontFamily: {
+        sans:['Poppins','sans-serif']
+      }
+    },
   },
   plugins: [],
 }
