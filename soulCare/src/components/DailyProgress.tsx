4f5e968bc219f804b6e985749536af21a095c115
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-07-06 16:02:48 +0530

    Structuring UserDashboard using components

---
+++ soulCare/src/components/DailyProgress.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DailyProgress = () => {
+  return (
+    <div>DailyProgress</div>
+  )
+}
+
+export default DailyProgress
---
+++ soulCare/src/components/HabbitTracker.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HabbitTracker = () => {
+  return (
+    <div>HabbitTracker</div>
+  )
+}
+
+export default HabbitTracker
