e14be9c569af947db4dcf83e61c363d13b6b029d
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-07-06 16:57:49 +0530

    Update Moodtracker, Meditation and HabbitTracker

--- soulCare/src/components/HabbitTracker.tsx
+++ soulCare/src/components/HabbitTracker.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { FaRunning, FaTint, FaUtensils } from "react-icons/fa";
 
-const HabbitTracker = () => {
+const HabitTracker: React.FC = () => {
   return (
-    <div>HabbitTracker</div>
-  )
-}
+    <div className="bg-cardBg p-4 rounded-xl">
+      <h3 className="font-semibold mb-2 text-heading">Habit Tracker</h3>
+      <ul className="space-y-2 text-sm">
+        <li>
+          <FaRunning className="inline-block mr-2 text-success" /> Morning run -
+          07:00am
+        </li>
+        <li>
+          <FaTint className="inline-block mr-2 text-primaryBtn" /> 1.5L of water
+          - All day
+        </li>
+        <li>
+          <FaUtensils className="inline-block mr-2 text-secondaryBtn" />{" "}
+          Mealprep - 11:00am
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default HabbitTracker
+export default HabitTracker;
