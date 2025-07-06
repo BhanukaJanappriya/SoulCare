73bc60cd698cc2d3323c6f01f00ad6deea043752
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-07-06 16:32:04 +0530

    adding react icons library

--- soulCare/src/components/MoodTracker.tsx
+++ soulCare/src/components/MoodTracker.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const MoodTracker = () => {
+const MoodTracker: React.FC = () => {
   return (
-    <div>MoodTracker</div>
-  )
-}
+    
+  );
+};
 
-export default MoodTracker
+export default MoodTracker;
