4fa3e6733059769e0ff0cbe6c2c5d05fa1b9dacf
Gayan-cmd <gayanrangana002@gmail.com>
2025-07-19 12:16:25 +0530

    Updated the login feature

--- soulCare/soulcare_backend/authapp/models.py
+++ soulCare/soulcare_backend/authapp/models.py
@@ -21,6 +21,7 @@ class User(AbstractUser):
 
 class PatientProfile(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
     nic = models.CharField(max_length=12)
     contact_number = models.CharField(max_length=15)
     address = models.TextField()
@@ -30,6 +31,7 @@ class PatientProfile(models.Model):
 
 class DoctorProfile(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
     nic = models.CharField(max_length=12)
     contact_number = models.CharField(max_length=15)
     specialization = models.CharField(max_length=255)
@@ -38,6 +40,7 @@ class DoctorProfile(models.Model):
 
 class CounselorProfile(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
     nic = models.CharField(max_length=12)
     contact_number = models.CharField(max_length=15)
     expertise = models.CharField(max_length=255)
