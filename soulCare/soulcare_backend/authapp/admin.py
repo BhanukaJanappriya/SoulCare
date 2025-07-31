1bb21ae4c3b7744580e863fc5f0fb4b13549f1cf
Gayan-cmd <gayanrangana002@gmail.com>
2025-07-31 22:22:00 +0530

    Implement the admin verifying feature to doctor and counselor accounts

--- soulCare/soulcare_backend/authapp/admin.py
+++ soulCare/soulcare_backend/authapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, PatientProfile, DoctorProfile, CounselorProfile
 
-# Register your models here.
+#  Optional: Show more fields in list view
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'role', 'is_verified', 'is_staff')
+    list_filter = ('role', 'is_verified')
+    actions = ['approve_selected_users']  #  Step 2: Add custom action
+
+    @admin.action(description='Approve selected users')
+    def approve_selected_users(self, request, queryset):
+        updated = queryset.filter(is_verified=False, role__in=['doctor', 'counselor','user']).update(is_verified=True)
+        self.message_user(request, f"{updated} user(s) successfully approved.")
+
+# Register models
+admin.site.register(User, UserAdmin)
+admin.site.register(PatientProfile)
+admin.site.register(DoctorProfile)
+admin.site.register(CounselorProfile)
+
